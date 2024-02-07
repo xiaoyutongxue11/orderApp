b1cfa8c18954d1e28848d4aeceda02641e43f631
tyy123 <1572235056@qq.com>
2024-02-07 13:54:01 +0800

    支付详情页头和尾

--- src/Components/ShoppingCar/Payment/Payment.js
+++ src/Components/ShoppingCar/Payment/Payment.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Payment.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import ShopCarContext from "../../../store/shopCarContext";
 const Payment = (props) => {
   const paymentRoot = document.getElementById("payment-root");
   const close = (e) => {
     e.stopPropagation();
     props.onClose();
   };
+  const shopCarCtx = useContext(ShopCarContext);
   return ReactDOM.createPortal(
-    <div className={classes.Payment}>
+    <div className={classes.PaymentOuter}>
       <FontAwesomeIcon icon={faClose} onClick={close}></FontAwesomeIcon>
+      <div className={classes.Payment}>
+        <header className={classes.Header}>餐品详情</header>
+        <div>列表</div>
+        <footer className={classes.Footer}>
+          <p className={classes.TotalPrice}>{shopCarCtx.totalPrice}</p>
+        </footer>
+      </div>
     </div>,
     paymentRoot
   );
