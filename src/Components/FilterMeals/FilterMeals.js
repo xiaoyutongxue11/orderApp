7c49da56d85a401112d4baf0b5c057b79f8d1464
tyy123 <1572235056@qq.com>
2024-02-11 23:43:48 +0800

    Effect返回回调函数实现输入框防抖效果

--- src/Components/FilterMeals/FilterMeals.js
+++ src/Components/FilterMeals/FilterMeals.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import classes from "./FilterMeals.module.css";
 const FilterMeals = (props) => {
+  const [keyword, setKeyword] = useState("");
   const inputKeyWord = (e) => {
-    props.onFilter(e.target.value.trim());
+    setKeyword(e.target.value.trim());
   };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      props.onFilter(keyword);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    }; 
+  }, [keyword,props]);
   return (
     <div className={classes.FilterMeals}>
       <div className={classes.SearchOuter}>
